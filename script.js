5f81f02a4bc834052d7c1431744a0ac6e43db761
Ayman Azougar <topayman5@gmail.com>
2022-03-06 22:05:00 +0100

    Saving script and animations

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 var IDtxt = document.getElementById("AdviceID");
 var quotetxt = document.getElementById("quote");
-// First of all we will need to generate a random number
 // Now let's Fetch the API
 function LoadAdvice() {
   fetch("https://api.adviceslip.com/advice")
@@ -10,5 +9,7 @@ function LoadAdvice() {
       quotetxt.innerHTML = data.slip.advice;
     });
 }
+// Load advice after clicking the button
 document.getElementById("dicebtn").addEventListener("click", LoadAdvice);
+// LOAD Advice when the browser refreshed or entred to the page
 LoadAdvice();
